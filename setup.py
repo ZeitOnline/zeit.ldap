c0cf1518a500751cb758178b2713a35ec4718942
Christian Zagrodnick <cz@gocept.com>
2011-11-13 18:16:32 +0000

    Back to development: 1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.ldap',
-    version='1.2.1',
+    version='1.2.2dev',
     author='Christian Zagrodnick',
     author_email='cz@gocept.com',
     description="""\
