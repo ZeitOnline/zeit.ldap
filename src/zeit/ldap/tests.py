2be864cbf9340423a6174ecf810bd6250b69c55f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-01-26 09:01:21 +0100

    Re ZON-2576: Update test setup

--- src/zeit/ldap/tests.py
+++ src/zeit/ldap/tests.py
@@ -5,7 +5,8 @@ import zeit.cms.testing
 import zeit.ldap.authentication
 
 
-LDAPLayer = zeit.cms.testing.ZCMLLayer('ftesting.zcml')
+LDAPLayer = zeit.cms.testing.ZCMLLayer(
+    'ftesting.zcml', product_config=zeit.cms.testing.cms_product_config)
 
 
 class FakeLdap(object):
