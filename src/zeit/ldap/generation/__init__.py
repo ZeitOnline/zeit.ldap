32a855d72ffdc271b5f75922f5d9fdf050f4bbce
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-12-09 11:05:33 +0100

    ZON-5594: Fix zope.generation dependency

--- src/zeit/ldap/generation/__init__.py
+++ src/zeit/ldap/generation/__init__.py
@@ -1,8 +1,7 @@
-
-import zope.app.generations
+import zope.generations.generations
 
 minimum_generation = 1
 generation = 1
 
-manager = zope.app.generations.generations.SchemaManager(
+manager = zope.generations.generations.SchemaManager(
     minimum_generation, generation, "zeit.ldap.generation")
