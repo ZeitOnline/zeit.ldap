9565f83d6198c722f5cf81076ab32aa86e36047c
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-02-05 13:53:31 +0100

    MAINT: Remove dead code (belongs to 4a0719d)

--- src/zeit/ldap/testing.py
+++ src/zeit/ldap/testing.py
@@ -1,5 +1,5 @@
-import zeit.cms.zope
 import zeit.cms.testing
+import zeit.cms.zope
 
 
 product_config = """
@@ -16,18 +16,6 @@ product_config = """
 
 CONFIG_LAYER = zeit.cms.testing.ProductConfigLayer(product_config, bases=(
     zeit.cms.testing.CONFIG_LAYER,))
-
-
-class CacheLayer(zeit.cms.testing.CacheLayer):
-
-    defaultBases = (CONFIG_LAYER,)
-
-    def setUp(self):
-        zeit.cms.zope.configure_dogpile_cache(None)
-
-
-DOGPILE_CACHE_LAYER = CacheLayer()
-
 ZCML_LAYER = zeit.cms.testing.ZCMLLayer(bases=(CONFIG_LAYER,))
 ZOPE_LAYER = zeit.cms.testing.ZopeLayer(bases=(ZCML_LAYER,))
 
