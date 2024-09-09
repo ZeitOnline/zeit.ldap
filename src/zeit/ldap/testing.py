db3bca1831a8a5915112da149764d1194023ac68
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-09-09 13:18:52 +0200

    MAINT: Set up product config via dict instead of str

--- src/zeit/ldap/testing.py
+++ src/zeit/ldap/testing.py
@@ -2,20 +2,15 @@ import zeit.cms.testing
 import zeit.cms.zope
 
 
-product_config = """
-<product-config zeit.ldap>
-  authenticator-plugins principalregistry
-  credentials-plugins xmlrpc-basic-auth
-
-  ad-tenant common
-  ad-client-id none
-  ad-client-secret none
-  ad-timeout 1
-</product-config>
-"""
-
-CONFIG_LAYER = zeit.cms.testing.ProductConfigLayer(product_config, bases=(
-    zeit.cms.testing.CONFIG_LAYER,))
+CONFIG_LAYER = zeit.cms.testing.ProductConfigLayer(
+    {
+        'authenticator-plugins': 'principalregistry',
+        'credentials-plugins': 'xmlrpc-basic-auth',
+        'ad-tenant': 'common',
+        'ad-client-id': 'none',
+        'ad-client-secret': 'none',
+        'ad-timeout': '1',
+    }, bases=(zeit.cms.testing.CONFIG_LAYER,))
 ZCML_LAYER = zeit.cms.testing.ZCMLLayer(bases=(CONFIG_LAYER,))
 ZOPE_LAYER = zeit.cms.testing.ZopeLayer(bases=(ZCML_LAYER,))
 
